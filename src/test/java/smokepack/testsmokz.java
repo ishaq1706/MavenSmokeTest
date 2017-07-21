8f1724528c3a6621b30a2b37b880cf747c47037f
ishaq1706 <ishaqshaik.1706@gmail.com>
2017-07-20 17:13:42 -0700

    maventest

---
+++ src/test/java/smokepack/testsmokz.java
@@ -0,0 +1,39 @@
+package smokepack;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class testsmokz {
+	
+	public static WebDriver driver;
+	public static CommonMethods Cm;
+	
+
+
+	 
+	@Test(groups="Smoke Test")
+	 
+	 public void test() throws MalformedURLException{
+		
+		 Cm=new CommonMethods(driver);
+		 driver =Cm.openBrowser("Chrome");
+		 Cm.navigateURL("http://10.1.10.238:8081/CounterWebApp/");
+		 Cm.verifyText("Maven Spring MVC Web Project Example");
+		 Cm.VerifyTitle("Maven Spring MVC Web Project Example");
+		// assertEquals("Maven Spring MVC Web Project Example", driver.findElement(By.xpath("/html/body/h1")).getText());
+		 System.out.println("Maven Spring MVC Web Project Example");
+		 Cm.closeBrowser();
+		
+		 
+		 
+		
+		
+		
+		
+		
+		
+	}
+
+}
